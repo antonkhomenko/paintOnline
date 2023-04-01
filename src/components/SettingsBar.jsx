26231f741111591d73b9786e6481070186cb6068
antonkhomenko <antonkhomenko5@gmail.com>
2023-04-01 04:30:49 +0300

    add ability to chose line width

--- src/components/SettingsBar.jsx
+++ src/components/SettingsBar.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import toolState from "../store/toolState.js";
 
 const SettingsBar = () => {
     return (
         <div className="settings-bar">
-            <input type="number" min={0} max={50}/>
+            <label htmlFor="line-width" style={{marginLeft: '10px'}}>Line width:</label>
+            <input
+                type="number"
+                min={1} max={50}
+                defaultValue={1} id="line-width"
+                style={{margin: '0 10px'}}
+                onChange={e => toolState.setLineWidth(e.target.value)}
+            />
         </div>
     );
 };
